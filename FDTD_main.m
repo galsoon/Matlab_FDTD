ee274032e54f7fbdcd0b19911d9a1b92b1b2acb3
Wonhak Son <galsoon@hanmail.net>
2022-05-07 16:33:36 +0900

    Hz Calculation  초기화 및 기본 출력 완료(소스코드 그대로 사용)

--- FDTD_main.m
+++ FDTD_main.m
@@ -6,6 +6,7 @@ mu0 = 4*pi*1e-7;
 c_light = 1/sqrt(eps0*mu0);
 freq = 2.0e9;
 len_tf = 0.5;
+number_of_materials = 2;
 
 L=[2.5 2.5];
 nx = 500;
@@ -18,8 +19,8 @@ dy = Y(ny) - Y(ny-1);
 dt = (1/c_light/sqrt(1/(dx^2)+1/(dy^2)))*0.99;
 
 Index = zeros(nx,ny);
-Index_X = zeros(nx,ny);
-Index_Y = zeros(nx,ny);
+IndexX = zeros(nx,ny);
+IndexY = zeros(nx,ny);
 
 coout = 100;
 Material = zeros(2,3);
@@ -78,23 +79,102 @@ Hz = zeros(nx,ny);
 Ex = zeros(nx,ny-1);
 Ey = zeros(nx-1,ny);
 
+k_Fz_1 = zeros(ny,1);
+k_Fz_2 = zeros(ny,1);
+k_Ez_1 = zeros(nx,1);
+k_Ez_2 = zeros(nx,1);
+k_Gx_1 = zeros(ny-1,1);
+k_Gx_2 = zeros(ny-1,1);
+k_Hx_1 = zeros(nx,1);
+k_Hx_2 = zeros(nx,1);
+k_Gy_1 = zeros(nx-1,1);
+k_Gy_2 = zeros(nx-1,1);
+k_Hy_1 = zeros(ny,1);
+k_Hy_2 = zeros(ny,1);
+k_Hz_1 = zeros(nx,1);
+k_Hz_2 = zeros(nx,1);
+k_Ex_1 = zeros(nx,1);
+k_Ex_2 = zeros(nx,1);
+k_Ey_1 = zeros(ny,1);
+k_Ey_2 = zeros(ny,1);
+
+k_Fz_1(:) = 1.0;
+k_Fz_2(:) = dt;
+k_Ez_1(:) = 1.0;
+k_Ez_2(:) = 1.0/eps0;
+k_Gx_1(:) = 1.0;
+k_Gx_2(:) = dt/dy;
+k_Hx_1(:) = 1.0/mu0;
+k_Hx_2(:) = 1.0/mu0;
+k_Gy_1(:) = 1.0;
+k_Gy_2(:) = dt/dx;
+k_Hy_1(:) = 1.0/mu0;
+k_Hy_2(:) = 1.0/mu0;
+k_Hz_1(:) = 1.0;
+k_Hz_2(:) = 1.0/mu0;
+k_Ex_1(:) = 2.0;
+k_Ex_2(:) = 2.0;
+k_Ey_1(:) = 2.0;
+k_Ey_2(:) = 2.0;
+
+K_a1(1:number_of_materials) = 1./(2*eps0*Material(1:number_of_materials,1)+Material(1:number_of_materials,3)*dt);
+K_b1(1:number_of_materials) = 2*eps0*Material(1:number_of_materials,1)-Material(1:number_of_materials,3)*dt;
+
+k_Fz_2 = repmat(k_Fz_2(2:ny-1)',nx-2,1);
+k_Gx_1 = repmat(k_Gx_1(1:ny-1)',nx,1);
+k_Gx_2 = repmat(k_Gx_2(1:ny-1)',nx,1);
+k_Gy_1 = repmat(k_Gy_1(1:nx-1),1,ny);
+k_Gy_2 = repmat(k_Gy_2(1:nx-1),1,ny);
+k_Hz_2 = repmat(k_Hz_2(2:nx-1),1,ny-2);
+k_Ey_1 = repmat(k_Ey_1(1:ny)',nx-1,1);
+k_Ey_2 = repmat(k_Ey_2(1:ny)',nx-1,1);
+
+
 figure('units','normalized','outerposition',[0 0 1 1]);
 set(gcf,'doublebuffer','on');
-tic
 
 
-for i=1:5
-    subplot(1,2,1);
-    pcolor(Y(1:ny),X(1:nx),Ez(1:nx,1:ny));
-    shading interp;
-    axis image;
-    colorbar;
+for T=1:500
+    Ey_1D(1:nx_b) = Ey_1D(1:nx_b) - k_Ey_b(1:nx_b).*(Hz_1D(2:nx_b+1)-Hz_1D(1:nx_b));
+    Hz_1D(1) = sin(2*pi*freq*(T-1)*dt);
+    Hz_1D(2:nx_b) = Hz_1D(2:nx_b) - k_Hz_b(2:nx_b).*(Ey_1D(2:nx_b)-Ey_1D(1:nx_b-1));
     
+    Fz_r1 = Wz(2:nx-1,2:ny-1);
+    Wz(2:nx-1,2:ny-1) = Wz(2:nx-1,2:ny-1) + k_Fz_2.*((Ex(2:nx-1,2:ny-1)-Ex(2:nx-1,1:ny-2))/dy - (Ey(2:nx-1,2:ny-1)-Ey(1:nx-2,2:ny-1))/dx);
+    Hz(2:nx-1,2:ny-1) = Hz(2:nx-1,2:ny-1) + k_Hz_2.*(Wz(2:nx-1,2:ny-1)-Fz_r1);
     
-    subplot(1,2,2);
-    pcolor(Y(1:ny),X(1:nx),Hz(1:nx,1:ny));
-    shading interp;
-    axis image;
-    colorbar;
+    Hz(nx_a+1,ny_a+1:ny_b+1) = Hz(nx_a+1,ny_a+1:ny_b+1) + dt./(mu0*Material(Index(nx_a+1,ny_a+1:ny_b+1)+1,2)'*dx)*Ey_1D(1);
+    Hz(nx_b+1,ny_a+1:ny_b+1) = Hz(nx_b+1,ny_a+1:ny_b+1) - dt./(mu0*Material(Index(nx_b+1,ny_a+1:ny_b+1)+1,2)'*dx)*Ey_1D(nx_b-nx_a+2);
+    
+    Gx_r1 = Mx(1:nx,1:ny-1);
+    Mx(1:nx,1:ny-1) = k_Gx_1.*Mx(1:nx,1:ny-1) + k_Gx_2.*(Hz(1:nx,2:ny)-Hz(1:nx,1:ny-1));
+    Ex(1:nx,1:ny-1) = K_a1(IndexX(1:nx,1:ny-1)+1).*(K_b1(IndexX(1:nx,1:ny-1)+1).*Ex(1:nx,1:ny-1)+k_Ex_1.*Mx(1:nx,1:ny-1)-k_Ex_2.*Gx_r1);
+    
+    Gy_r1 = My(1:nx-1,1:ny);
+    My(1:nx-1,1:ny) = k_Gy_1.*My(1:nx-1,1:ny)-k_Gy_2.*(Hz(2:nx,1:ny)-Hz(1:nx-1,1:ny));
+    Ey(1:nx-1,1:ny) = K_a1(IndexY(1:nx-1,1:ny)+1).*(K_b1(IndexY(1:nx-1,1:ny)+1).*Ey(1:nx-1,1:ny)+k_Ey_1.*My(1:nx-1,1:ny)-k_Ey_2.*Gy_r1);
+    
+    Ex(nx_a+1:nx_b+1,ny_a) = Ex(nx_a+1:nx_b+1,ny_a) - 2*dt/dy*K_a1(Index(nx_a+1:nx_b+1,ny_a)+1)'.*Hz_1D(2:(nx_b-nx_a+2));
+    Ex(nx_a+1:nx_b+1,ny_b+1) = Ex(nx_a+1:nx_b+1,ny_b+1) + 2*dt/dy*K_a1(Index(nx_a+1:nx_b+1,ny_b+1)+1)'.*Hz_1D(2:(nx_b-nx_a+2));
+    
+    Ey(nx_a,ny_a+1:ny_b+1) = Ey(nx_a,ny_a+1:ny_b+1) + 2*dt/dx*K_a1(Index(nx_a,ny_a+1:ny_b+1)+1,1)'.*Hz_1D(2);
+    Ey(nx_b+1,ny_a+1:ny_b+1) = Ey(nx_b+1,ny_a+1:ny_b+1) - 2*dt/dx*K_a1(Index(nx_b+1,ny_a+1:ny_b+1)+1,1)'.*Hz_1D(nx_b-nx_a+2);
+
+    
+    if(mod(T,10) == 0)
+        
+%         subplot(1,2,1);
+%         pcolor(Y(1:ny),X(1:nx),Ez(1:nx,1:ny));
+%         shading interp;
+%         axis image;
+%         colorbar;
+%         
+        subplot(1,2,2);
+        pcolor(Y(1:ny),X(1:nx),Hz(1:nx,1:ny));
+        shading interp;
+        axis image;
+        colorbar;
+        drawnow;
+    end
 end
 
